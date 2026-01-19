563336d7483d6f906ab6680914757050e23c004c
harshalsojitra <hssojitra@gmail.com>
2026-01-19 17:38:23 +0100

    Fix: include app source code correctly

---
+++ app/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+import os
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    # os.getenv reads the variables we put in deployment.yaml
+    env_name = os.getenv("APP_ENVIRONMENT", "Local-Machine")
+    db_pass = os.getenv("DATABASE_PASSWORD", "No-Secret-Found")
+    
+    return {
+        "message": f"Hello from the {env_name}!",
+        "secret_status": "Secret Loaded" if db_pass == "super-secret-password-123" else "Secret Missing",
+        "version": "2.0.0",
+        "status": "Running"
+    }
+
+@app.get("/healthz")
+def healthz():
+    return {"status": "healthy"}
